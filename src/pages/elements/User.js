78ddc460f99aca47d7ea43de84d3c59299c3eec9
sdlhopol <sdlhopol@redhat.com>
2019-05-24 13:44:40 -0400

    expand gpg_key button
    added a button that allows the user to expand or shrink the gpg_code as an alternative to downloading it

--- src/pages/elements/User.js
+++ src/pages/elements/User.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import { ClipboardCopy, ClipboardCopyVariant, Grid, GridItem, Button, Tooltip } from '@patternfly/react-core';
-import { ExportIcon } from '@patternfly/react-icons';
+import React, { useState }from 'react';
 import Definition from '../../components/Definition';
 import Roles from './Roles';
 
 function User({ user }) {
-  let key;
+  let downloadKeyButton;
+  let showMoreKey;
+  let keyState;
   if (user.public_gpg_key == null) {
-    key = '-';
+    keyState= '-';
+    downloadKeyButton = '';
+    showMoreKey = '';
   } else if (user.public_gpg_key.length >= 50) {
-    key = (
-      <button type="button" onClick={downloadKey}>
-        {user.public_gpg_key.substring(0, 50)}
-      </button>
+      keyState= user.public_gpg_key.substring(0, 50)
+      downloadKeyButton =  (
+        <button type="button" onClick={downloadKey}>
+          Download Key
+        </button>
     );
+      showMoreKey = (
+        <button type="button" onClick={expandKey}>
+          Show More
+       </button>
+    )
   } else {
-    key = (
+    keyState=user.public_gpg_key
+    downloadKeyButton =  (
       <button type="button" onClick={downloadKey}>
-        {user.public_gpg_key}
+        Download Key
       </button>
     );
   }
+  const [key, changeKey] = useState(keyState);
+  const [showMoreButton, changeButton] = useState(showMoreKey);
+
   // taken from: https://stackoverflow.com/questions/44656610/download-a-string-as-txt-file-in-react
   // there probably is a better way in react to do this
   function downloadKey(e) {
@@ -33,6 +45,27 @@ function User({ user }) {
       document.body.appendChild(element);
       element.click();
     }
+  }
+  function expandKey (e) {
+    e.preventDefault();
+    changeKey(user.public_gpg_key);
+    changeButton(
+      <button type="button" onClick={shrinkKey}>
+        Show Less
+      </button>
+    );
+    console.log(showMoreButton);
+  }
+  function shrinkKey(e){
+    e.preventDefault();
+    changeKey(user.public_gpg_key.substring(0,50));
+    changeButton(
+      <button type="button" onClick={expandKey}>
+          Show More
+       </button>
+    );
+    console.log(showMoreButton);
+
   }
   return (
     <React.Fragment>
@@ -71,7 +104,9 @@ function User({ user }) {
             )) ||
               '-'
           ],
-          ['Public gpg Key', key]
+          ['Public gpg Key', key ],
+          ['-', showMoreButton],
+          ['', downloadKeyButton]
         ]}
       />
       <h4>Roles</h4>
