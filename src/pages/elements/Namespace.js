89115f54a0025c58a682d151baef55f4c0b756eb
sdlhopol <sdlhopol@redhat.com>
2019-06-03 14:35:57 -0400

    grafana links for clusters
    clusters with custom dashboards on grafana can now link to those dashboards

--- src/pages/elements/Namespace.js
+++ src/pages/elements/Namespace.js
@@ -4,6 +4,19 @@ import GrafanaUrl from './GrafanaUrl';
 import Definition from '../../components/Definition';
 
 function Namespace({ namespace }) {
+  let grafana;
+  if (namespace.grafanaUrl !== null) {
+    grafana = (
+      <a href={namespace.grafanaUrl} target="_blank" rel="noopener noreferrer">
+        Link
+      </a>
+    );
+  } else {
+    grafana = (
+      <GrafanaUrl jumpHost={namespace.cluster.jumpHost} cluster={namespace.cluster.name} namespace={namespace.name} />
+    );
+  }
+  console.log(grafana);
   return (
     <React.Fragment>
       <h4>Info</h4>
