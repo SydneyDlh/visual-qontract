7c075f642c46631001455be5fa262cd89a76de3b
sdlhopol <sdlhopol@redhat.com>
2019-06-03 14:35:57 -0400

    grafana links for clusters
    clusters with custom dashboards on grafana can now link to those dashboards

--- src/pages/elements/Namespace.js
+++ src/pages/elements/Namespace.js
@@ -9,10 +9,14 @@ function Namespace({ namespace }) {
     grafana = (
       <a href={namespace.grafanaUrl} target="_blank" rel="noopener noreferrer">
 <<<<<<< HEAD
+<<<<<<< HEAD
         Link
 =======
         Grafana
 >>>>>>> 355d8c2... added grafana links to custom dashbaords
+=======
+        Link
+>>>>>>> 7fa5219... grafana links for clusters
       </a>
     );
   } else {
