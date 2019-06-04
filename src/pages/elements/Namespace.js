33bb55539d59b2527cf8a8b73c486e078eb10e53
sdlhopol <sdlhopol@redhat.com>
2019-06-04 10:21:59 -0400

    Revert "fixing up"
    This reverts commit 9b15a14db72419ee7af81a888c3c1c10767ef750.

--- src/pages/elements/Namespace.js
+++ src/pages/elements/Namespace.js
@@ -8,7 +8,15 @@ function Namespace({ namespace }) {
   if (namespace.grafanaUrl !== null) {
     grafana = (
       <a href={namespace.grafanaUrl} target="_blank" rel="noopener noreferrer">
+<<<<<<< HEAD
+<<<<<<< HEAD
         Link
+=======
+        Grafana
+>>>>>>> 355d8c2... added grafana links to custom dashbaords
+=======
+        Link
+>>>>>>> 7fa5219... grafana links for clusters
       </a>
     );
   } else {
