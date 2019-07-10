fd84e99106528ab4c7c577b10ab6c43b28ee6682
sdlhopol <sdlhopol@redhat.com>
2019-07-10 15:57:51 -0400

    fragmentTypes

--- src/fragmentTypesGenerator.js
+++ src/fragmentTypesGenerator.js
@@ -1,11 +1,19 @@
 const fetch = require('node-fetch');
 const fs = require('fs');
 
-fetch('http://localhost:4000/graphql', {
+const url = process.env.GRAPHQL_URI || '/graphql';
+
+const headers = { 'Content-Type': 'application/json' };
+if (typeof process.env.AUTHORIZATION !== 'undefined') {
+  headers.Authorization = process.env.AUTORIZATION;
+}
+
+fetch(url, {
   method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
+  headers,
   body: JSON.stringify({
     variables: {},
+    headers,
     query: `
       {
         __schema {
@@ -18,15 +26,13 @@ fetch('http://localhost:4000/graphql', {
           }
         }
       }
-    `,
-  }),
+    `
+  })
 })
   .then(result => result.json())
   .then(result => {
     // here we're filtering out any type information unrelated to unions or interfaces
-    const filteredData = result.data.__schema.types.filter(
-      type => type.possibleTypes !== null,
-    );
+    const filteredData = result.data.__schema.types.filter(type => type.possibleTypes !== null);
     result.data.__schema.types = filteredData;
     fs.writeFileSync('./src/fragmentTypes.json', JSON.stringify(result.data), err => {
       if (err) {
